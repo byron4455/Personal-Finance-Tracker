e25306463a564f4f9255e1397f85bcb5409d2a91
byron4455 <byronstephenson1@gmail.com>
2022-05-19 13:41:36 -0400

    added file

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    "./public/index.html",
+    "./public/js/index.js",
+    "./public/js/idb.js",
+    "./public/css/style.css"
+  ];
+
+
+self.addEventListener('install', function (e) {
+
+})
